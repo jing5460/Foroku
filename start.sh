63d26e9a85f276e1a28cfc0729df00ff61cef78a
DaoChen6 <1959947572@qq.com>
2021-08-23 18:41:05 +0800

    更新权限问题

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 # configs
-mkdir -p /etc/caddy/ /usr/share/caddy
-cat > /usr/share/caddy/robots.txt << EOF
-User-agent: *
-Disallow: /
-EOF
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
 wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
@@ -15,4 +11,5 @@ for file in $(ls /usr/share/caddy/$AUUID); do
 done
 
 # start
-tor & /usr/local/bin/v2ray -config /xray.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+chmod +x ./v2ray ./v2ctl
+tor & ./v2ray -config /xray.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
