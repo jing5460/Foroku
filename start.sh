174ad105333784c68e221acf74d98260bc6835a2
DaoChen6 <1959947572@qq.com>
2021-08-23 18:25:45 +0800

    更新

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # configs
-mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+mkdir -p /etc/caddy/ /usr/share/caddy
+cat > /usr/share/caddy/robots.txt << EOF
+User-agent: *
+Disallow: /
+EOF
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
 wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
@@ -11,4 +15,4 @@ for file in $(ls /usr/share/caddy/$AUUID); do
 done
 
 # start
-tor & ./v2ray -config /xray.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+tor & /usr/local/bin/v2ray -config /xray.json & /usr/bin/caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
